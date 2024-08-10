13c663dee845402f05b9e42c59d707a388851090
Maggie H <hillebrecht@chapman.edu>
2024-08-09 18:14:23 -0700

    update

---
+++ CardWorkout-Programmatic/CWButton.swift
@@ -0,0 +1,20 @@
+//
+//  CWButton.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Maggie Hillebrecht on 8/4/24.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
